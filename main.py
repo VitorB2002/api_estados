f44589b3de4ea8e9683e2b8f990ba5f1055dece7
VitorB2002 <vitorb.santos2002@gmail.com>
2023-11-17 01:09:45 -0300

    fix(main.py): lidando com CORS

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 import constants
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
 ESTADOS = constants.estados
 DETALHES = constants.detalhes
 
